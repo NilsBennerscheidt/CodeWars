b8b2df009cf58c65545217e1cfd3f0d047f375f5
NilsBennerscheidt <37406580+NilsBennerscheidt@users.noreply.github.com>
2019-06-25 19:48:13 +0200

    First Commit

---
+++ src/KYU7/Main.java
@@ -0,0 +1,12 @@
+package KYU7;
+
+public class Main {
+
+    public static void main(String[] args){
+        String a = "Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn";
+        a=Meeting.meeting(a);
+        System.out.println(a);
+
+    }
+
+}
---
+++ src/KYU7/Matrix.java
@@ -0,0 +1,16 @@
+package KYU7;
+
+public class Matrix {
+    public static int[][] matrixAddition(int[][] a, int[][] b) {
+
+        for(int i = 0; i<a.length; i++){
+            for(int y = 0; y<a[i].length;y++){
+                a[i][y] =a[i][y]+b[i][y];
+            }
+        }
+
+
+
+        return a;
+    }
+}
